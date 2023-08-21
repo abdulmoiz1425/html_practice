f20a380395670b17584b6cc6fc3518883759e3ca
abdulmannan <Mannan>
2023-08-21 14:24:54 +0500

    first commit

---
+++ practice1.js
@@ -0,0 +1,37 @@
+// console.log("hello world")
+// let a=15
+// let b=10
+// console.log(a+b)
+// console.log("++a=",++a)
+// // console.log("a++=",a++)
+// console.log("a--=",a--)
+// console.log(--a)
+// console.log("a=",a)
+// b=54
+// c=26
+
+// age=prompt("what is your age?")
+// let d=20
+// d=age>=18? "you can drive" : "you cannot drive"
+// console.log(d)
+// 
+const sum=(a,d)=>{
+    return a+d
+}
+console.log(sum)
+a=2
+d=5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
